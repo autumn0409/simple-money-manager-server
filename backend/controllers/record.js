212d4881d7508b4890cff673c3fcc1aa2ad152ea
Adam Su <su850409@gmail.com>
2020-10-09 02:13:42 +0800

    add "getRecords"

--- backend/controllers/record.js
+++ backend/controllers/record.js
@@ -1,10 +1,92 @@
+const sequelize = require("sequelize");
 const db = require("../models");
 
 const Category = db.category;
 const Record = db.record;
 
 module.exports = {
-  getRecords: async (req, res) => {},
+  getRecords: async (req, res) => {
+    let income = 0;
+    let expenses = 0;
+    const { month, year } = req.query;
+
+    try {
+      // query records
+      const records = await Record.findAll({
+        where: {
+          [sequelize.Op.and]: [
+            sequelize.where(sequelize.fn("YEAR", sequelize.col("date")), year),
+            sequelize.where(
+              sequelize.fn("MONTH", sequelize.col("date")),
+              month
+            ),
+          ],
+        },
+        include: [
+          { model: Category, attributes: ["type", "name"], required: true },
+        ],
+      });
+
+      let dailyRecords = {};
+
+      // process records one by one
+      records.forEach((record) => {
+        // get income and expenses of the record
+        let recordExpenses = 0;
+        let recordIncome = 0;
+
+        if (record.category.type === "expenses") {
+          recordExpenses = record.amount;
+        } else if (record.category.type === "income") {
+          recordIncome = record.amount;
+        }
+
+        income += recordIncome;
+        expenses += recordExpenses;
+
+        // create recordItem
+        const dateOfRecord = new Date(record.date).getDate();
+        const recordItem = {
+          date: record.date,
+          amount: record.amount,
+          paymentMethod: record.paymentMethod,
+          id: record.id,
+          remarks: record.remarks,
+          type: record.category.type,
+          category: record.category.name,
+        };
+
+        // insert recordItem into dailyRecords
+        if (!(dateOfRecord in dailyRecords)) {
+          const dailyRecordObj = {
+            income: recordIncome,
+            expenses: recordExpenses,
+            recordItems: [recordItem],
+          };
+          dailyRecords[dateOfRecord] = dailyRecordObj;
+        } else {
+          dailyRecords[dateOfRecord].recordItems.push(recordItem);
+          dailyRecords[dateOfRecord].income += recordIncome;
+          dailyRecords[dateOfRecord].expenses += recordExpenses;
+        }
+      });
+
+      // only keep values in dailyRecords
+      dailyRecords = Object.keys(dailyRecords).map((date) => {
+        return dailyRecords[date];
+      });
+
+      const result = {
+        month,
+        income,
+        expenses,
+        dailyRecords,
+      };
+      res.status(200).json(result);
+    } catch (err) {
+      res.status(400).send("Get Error");
+    }
+  },
 
   createRecord: async (req, res) => {
     const { date, amount, type, category, paymentMethod, remarks } = req.body;
