7e06c84501d04637593d1fef1d27209b921b78bb
Adam Su <su850409@gmail.com>
2020-10-09 14:21:09 +0800

    add cors

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 // express app
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 // listen
 const port = process.env.PORT || 3000;
